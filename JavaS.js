37cba45f7353c8c11cff426c67b9daa8d08de272
avilashpopo <avilashpopo774@gmail.com>
2023-07-09 23:13:00 +0530

    final project commit

--- JavaS.js
+++ JavaS.js
@@ -16,3 +16,33 @@ const sr = ScrollReveal ({
 sr.reveal('.hero-text' ,{delay: 200, origin: 'top'});
 sr.reveal('.hero-img' ,{delay: 450, origin: 'top'});
 sr.reveal('.icons' ,{delay: 500, origin: 'left'});
+sr.reveal('.column.side img' ,{delay: 200, origin: 'left'});
+sr.reveal('.about h1' ,{delay: 200, origin: 'left'});
+sr.reveal('.about h1 hr' ,{delay: 200, origin: 'left'});
+sr.reveal('.box' ,{delay: 100, origin: 'left'});
+
+// navbar transparency
+
+const headerEl = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        headerEl.classList.add('header-scrolled');
+    }
+    else if (window.scrollY <= 50) {
+        headerEl.classList.remove('header-scrolled');
+    }
+});
+
+// function changeBg() {
+//     var navbar2 = documnet.querySelector('.header');
+//     var scrollValue = window.scrollY;
+//     if(scrollValue < 50) {
+//         navbar2.classList.remove('header-scrolled');
+//     }
+//     else {
+//         navbar2.classList.add('header-scrolled');
+//     }
+// }
+
+// window.addEventListener('scroll', changeBg);
